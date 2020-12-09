8dd867cce39d0e3899e69cd718cffc56c91a3c18
AntoineBardy <antoine.bardy11@gmail.com>
2020-12-09 11:39:25 +0100

    Update ExamPython.py
    Je décide d'essayer sans fonction car je trouve cela plus simple.

--- ExamPython.py
+++ ExamPython.py
@@ -6,3 +6,6 @@ from colorama import Fore, Back, Style
 listeMot = [ "examen" , "python" , "bouche" , "cheval" , "purger" , "souris", "france", "bateau" , "animal" , "bronze" ]
 Mot = random.choice(listeMot)
 print (Mot)
+
+for Tentative in range (0,8):
+	JoueurTry = input("Saisir un mot de 6 lettre uniquement : ") 
