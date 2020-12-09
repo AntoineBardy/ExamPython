5c1c9a7b8bfa01a73ccfbe0ca9c84994ee1eb0e4
AntoineBardy <antoine.bardy11@gmail.com>
2020-12-09 10:14:28 +0100

    Create ExamPython.py

---
+++ ExamPython.py
@@ -0,0 +1,23 @@
+from colorama import init
+init()
+from colorama import Fore, Back, Style
+
+mot1 = "bronze"
+mot2 = "bateau"
+mot3 = "python"
+mot4 = "france"
+mot5 = "bouche"
+mot6 = "examen"
+mot7 = "purger"
+mot8 = "souris"
+mot9 = "cheval"
+mot10 = "animal"
+
+mots = (mot1 or mot2 or mot3 or mot4 or mot5 or mot6 or mot7 or mot8 or mot9 or mot10)
+#fonctions
+def zoneDeJeu:
+	
+def chercheLettre:
+	mots = 
+
+	pass
