d3f01e4c83918661fcf4a374b309ce99703d210a
AntoineBardy <antoine.bardy11@gmail.com>
2020-12-09 10:35:35 +0100

    Update ExamPython.py

--- ExamPython.py
+++ ExamPython.py
@@ -13,12 +13,12 @@ mot8 = "souris"
 mot9 = "cheval"
 mot10 = "animal"
 
-zone = ["0", "1", "2", "3", "4", "5", "6"]
-#mots = (mot1 or mot2 or mot3 or mot4 or mot5 or mot6 or mot7 or mot8 or mot9 or mot10)
-#fonctions
-def zoneDeJeu (zone):
-	compteur = 0
-	for compteur in range (0,6):
-		grille[compteur] = "_"
-	return
+tentatives = 8 
 
+def chercheLettre (motZero):
+	for i in range mots;
+		if lettre == mots[i];
+		return i
+
+chercheLettre (mot1)
+input()
