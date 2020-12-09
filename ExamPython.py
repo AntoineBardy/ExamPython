dce983ae6c412b4876f769fb462f6cce373f801a
AntoineBardy <antoine.bardy11@gmail.com>
2020-12-09 11:45:24 +0100

    Update ExamPython.py

--- ExamPython.py
+++ ExamPython.py
@@ -8,4 +8,10 @@ Mot = random.choice(listeMot)
 print (Mot)
 
 for Tentative in range (0,8):
-	JoueurTry = input("Saisir un mot de 6 lettre uniquement : ") 
+	JoueurTry = input("Saisir un mot de 6 lettre uniquement : ")
+	for i in range (0,6):
+		if mot[i] == JoueurTry [i]
+			print(Fore.RED + , JoueurTry)
+		else : 
+			couleur : False
+			
