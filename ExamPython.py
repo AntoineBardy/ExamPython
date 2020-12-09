5a5ccee13b16210a1662ca370274741e815e1c04
AntoineBardy <antoine.bardy11@gmail.com>
2020-12-09 11:36:24 +0100

    Update ExamPython.py
    Je me suis permis de recommencer à 0 car mon code ne marchait pas et je voulais tenter une autre technique.

--- ExamPython.py
+++ ExamPython.py
@@ -6,23 +6,3 @@ from colorama import Fore, Back, Style
 listeMot = [ "examen" , "python" , "bouche" , "cheval" , "purger" , "souris", "france", "bateau" , "animal" , "bronze" ]
 Mot = random.choice(listeMot)
 print (Mot)
-
-def chercheLettre (lettre, mot):
-	for i in range (len(mots)):
-		if lettre == mots[i]:
-		return
-
-def oneLettre (lettre, indiceLettre, mot):
-	if lettre in mot:
-		if mot[indiceLettre] == motChoisis[indiceLettre]:
-			print(Fore.RED + lettre, end=" ")
-			return
-		print(Fore.YELLOW + lettre, end=" ")
-		return
-	print(Fore.BLUE + lettre, end=" ")
-
-
-def nombreDeFois (lettre, mot):
-	for i in range(len(mots)):
-		chercheLettre (lettre)
-		print ()
