d3e210e604f3cd8b5ce6f410ccd95e9f44860569
AntoineBardy <antoine.bardy11@gmail.com>
2020-12-09 12:01:06 +0100

    Update ExamPython.py

--- ExamPython.py
+++ ExamPython.py
@@ -4,19 +4,27 @@ init()
 from colorama import Fore, Back, Style
 
 listeMot = [ "examen" , "python" , "bouche" , "cheval" , "purger" , "souris", "france", "bateau" , "animal" , "bronze" ]
-Mot = random.choice(listeMot)
+mot = random.choice(listeMot)
 print (Mot)
 
 for Tentative in range (0,8):
 	JoueurTry = input("Saisir un mot de 6 lettre uniquement : ")
 	for i in range (0,6):
-		if mot[i] == JoueurTry [i]
-			print(Fore.RED + , JoueurTry[i])
+		if mot[i] == JoueurTry[i]:
+			print(Fore.RED + JoueurTry[i])
 		else : 
-			couleur : False
+			couleur = False
 			for j in range (0,6):
-				if mot[i] == JoueurTry[j]
-					print(Fore.YELLOW + , JoueurTry[j])
-					couleur : True
+				if mot[i] == JoueurTry[j]:
+					print(Fore.YELLOW + JoueurTry[j])
+					couleur = True
 			if couleur : False
-				print (Fore.BLUE + JoueurTry[j])
+			    print(Fore.BLUE + JoueurTry[j])
+				
+	if mot == JoueurTry
+	print ("Bravo, vous avez trouvez le mot !")
+
+	elif mot != JoueurTry
+	print ("Vous avez perdu le mot était", mot)
+
+input()
